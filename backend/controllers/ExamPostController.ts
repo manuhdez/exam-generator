a4f48802b7019de591e1d3b27c01b1b5c3d69a9b
Manuel Hernández <manuel@secret-source.eu>
2021-05-29 20:47:12 +0100

    fix: update backend logging error

--- backend/controllers/ExamPostController.ts
+++ backend/controllers/ExamPostController.ts
@@ -4,14 +4,13 @@ import App from "../Context/App/App";
 
 export default class ExamPostController {
   public static async invoke(req: Request, res: Response) {
-    try {
       const { title, uri } = req.body;
       console.log({ title, uri })
       const exam = new Exam(title, uri);
       console.log({ exam })
       const app = new App();
+    try {
       const [report] = await app.run([exam]);
-
       res.status(201).json(report);
     } catch (err) {
       res.status(500).json({ err });
